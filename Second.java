14866995ab0e3fabc73463475af89cf3600ce1f5
yashshinde70 <100059970+yashshinde70@users.noreply.github.com>
2024-11-30 15:51:12 +0530

    Update Second.java

--- Second.java
+++ Second.java
@@ -4,7 +4,19 @@ public static void main(String[] args){
 
 System.out.printl("Welcome");
 System.out.printl("Yash shinde");
-
+System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");System.out.printl("Welcome");
+System.out.printl("Yash shinde");
 System.out.println("jack");
 System.out.println();
 
